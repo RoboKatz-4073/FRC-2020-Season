97fef07c02ce3c7bb79f308f9fcf79a9ebab4092
JoeTelaa0 <47280152+The1TrueJoe@users.noreply.github.com>
2019-12-19 22:39:55 -0500

    Update talk.sh

--- pi/util/talk.sh
+++ pi/util/talk.sh
@@ -3,6 +3,6 @@
 # Bootup sound for Team 4073's JoeBot 2.0
 # Written by Joseph Telaak
 
-espeak "I am the Joe-Bot 2.0, I will destroy you puny humans"
+espeak "I am the Joe-Bot 2.0"
 mplayer sans.mp3
 mplayer sans-full.mp3
