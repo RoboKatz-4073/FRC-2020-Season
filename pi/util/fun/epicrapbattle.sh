8a8779fb56f796be5d7055048a8f66fa9ea3182d
FRC 4073 <58819472+FRC-4073@users.noreply.github.com>
2020-01-13 17:37:21 -0500

    Add epic rap battle

---
+++ pi/util/fun/epicrapbattle.sh
@@ -0,0 +1,43 @@
+#This is a recreation of the Epic Rap Battles of History 
+#Stephen Hawking vs Albert Einstein
+#
+#By Joseph Telaak, FRC-4073 RoboKatz
+
+
+#Albert Einstein
+espeak "When I apply my battle theory"
+espeak "Minds are relatively blown"
+espeak "So take a seat Steve"
+espeak "Oops  I see you brought your own"                                          
+espeak "What's with your voice"
+espeak "I can't frickin tell"                                                      
+espeak "You sound like WALL-E"
+espeak "Having sex with a Speak and Spell"
+espeak "I'll school you anywhere"
+espeak "MIT to Oxford"
+espeak "All your fans will be like"
+espeak "Um that was Hawkward"                                                      
+espeak "I'm as dope as two rappers, you better be scared" 
+espeak "Because that means Albert E. equals MC squared"
+
+#Stephen Hawking
+espeak "You've got no idea what your messing with here boy"
+espeak "I've got 12 inch rims on my chair that's how I roll y'all"
+espeak "You look like someone glued a mustache on a troll doll"
+espeak "I'll be stretchin' out the rhyme like gravity stretches time"
+espeak "When you try to put your little pea brain against this kind of mind"
+espeak "I'm the best, I'm the Snoop Dogg of Science"\
+espeak "I'll be dropping mad apples on your head from the shoulders of giants"
+
+#Albert Einstein
+espeak "I'm the giant whose shoulders you'd have stood on if you could stand"
+espeak "I'll give you a brief history of pain with the back of my hand"
+espeak "You can't destroy matter or me for serious"
+espeak "Ripping holes in you bigger than the hole in your black hole theory was"
+
+#Stephen Hawking
+espeak "There are ten million million million million million million million million million particles in the universe that we can observe, Your mama took the ugly one's and put them into one nerd"
+espeak "You wanna bring the heat with the mushroom clouds you're making"
+espeak "I'm about to bake raps from scratch like Carl Sagan"
+espeak "While it's true that my work is based on you"
+espeak "I'm a super-computer you're like a TI-82"
