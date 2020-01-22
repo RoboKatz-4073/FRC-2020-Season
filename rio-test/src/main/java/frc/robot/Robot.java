c9236a2b6696c635a5fa29c593d7bdcdb936b674
JoeTelaa0 <47280152+The1TrueJoe@users.noreply.github.com>
2020-01-22 18:03:01 -0500

    Add controllers

--- rio-test/src/main/java/frc/robot/Robot.java
+++ rio-test/src/main/java/frc/robot/Robot.java
@@ -236,6 +236,219 @@ public class Robot extends TimedRobot {
   /**
    * This function is called periodically during test mode.
    */
+//  @Override
+//  public void testPeriodic() {
+//
+//    double speed = .25;
+//
+//    double Xstick = m_stickboi.getRawAxis(0) * Speed;
+//    double Ystick = m_stickboi.getRawAxis(1) * Speed;
+//    double XX     = m_stickboi.getRawAxis(4) * Speed;
+//
+//     if (Ystick < 0.2 && Ystick > -0.2) {
+//
+//      Ystick = 0;
+//
+//    }
+//
+//    if (Xstick < 0.2 && Xstick > -0.2) {
+//
+//    Xstick = 0;
+//
+//    } 
+//
+//    if (m_stickboi.getRawAxis(4) > 0.2 || m_stickboi.getRawAxis(4) < -0.2) {
+//
+//    RightFront.set(ControlMode.PercentOutput, XX);
+//    LeftFront.set(ControlMode.PercentOutput, XX);
+//    RightBack.set(ControlMode.PercentOutput, -XX * 1.07);
+//    LeftBack.set(ControlMode.PercentOutput, -XX);
+//
+//    } else {
+//
+//    RightFront.set(ControlMode.PercentOutput, Ystick + Xstick);
+//    LeftFront.set(ControlMode.PercentOutput, -Ystick + Xstick);
+//    RightBack.set(ControlMode.PercentOutput, Ystick + Xstick);
+//    LeftBack.set(ControlMode.PercentOutput, -Ystick + Xstick);
+//
+//    }
+//  }
+//
+//}
+
+
+ @Override
+  public void teleopPeriodic() {
+
+    if (m_buttonboi.getYButton()) {
+
+      operationMode = 2;
+
+    } else if (m_buttonboi.getBButton()) {
+
+      operationMode = 1;
+
+    }
+    
+    if (operationMode == 1) {
+
+      teleopOperationModeOne();
+
+    } else if (operationMode == 2) {
+
+      teleopOperationModeTwo();
+
+    }
+
+    if (m_buttonboi.getAButton()) {
+
+    } else if (m_buttonboi.getXButton()) {
+
+    }
+    
+  }
+
+  public void teleopOperationModeOne() {
+
+    double Speed = 0.50;
+
+    // Macros
+    if (m_stickboi.getYButton()) {
+
+      // 15% Speed
+      Speed = 0.50;
+
+    } else if (m_stickboi.getBButton()) {
+
+      // 25% Speed
+      Speed = 0.25;
+
+    } else if (m_stickboi.getAButton()) {
+
+      // 75% Speed
+      Speed = 0.15;
+
+    } else if (m_stickboi.getXButton()) {
+
+      // Max Speed
+      Speed = 1;
+
+    } else if (m_stickboi.getBumper(Hand.kRight)) {
+
+      // Spin
+      Spin.Spin180(1);
+
+    } else if (m_stickboi.getBumper(Hand.kLeft)) {
+
+      // Spin
+      Spin.Spin180(2);
+
+    } else if (m_stickboi.getStartButton()) {
+
+      // BeepBoop
+      BeepBoop.beepboop();
+
+    } else if (Math.round(m_stickboi.getTriggerAxis(Hand.kRight)) == 1) {
+
+    } else if (Math.round(m_stickboi.getTriggerAxis(Hand.kRight)) == 1) {
+      
+    }
+
+    double Xstick = m_stickboi.getRawAxis(0) * Speed;
+    double Ystick = m_stickboi.getRawAxis(1) * Speed;
+    double XX     = m_stickboi.getRawAxis(4) * Speed;
+
+     if (Ystick < 0.2 && Ystick > -0.2) {
+
+      Ystick = 0;
+
+    }
+
+    if (Xstick < 0.2 && Xstick > -0.2) {
+
+      Xstick = 0;
+
+    } 
+
+    if (m_stickboi.getRawAxis(4) > 0.2 || m_stickboi.getRawAxis(4) < -0.2) {
+
+    RightFront.set(ControlMode.PercentOutput, XX);
+    LeftFront.set(ControlMode.PercentOutput, XX);
+    RightBack.set(ControlMode.PercentOutput, -XX * 1.07);
+    LeftBack.set(ControlMode.PercentOutput, -XX);
+
+    } else {
+
+    RightFront.set(ControlMode.PercentOutput, Ystick + Xstick);
+    LeftFront.set(ControlMode.PercentOutput, -Ystick + Xstick);
+    RightBack.set(ControlMode.PercentOutput, Ystick + Xstick);
+    LeftBack.set(ControlMode.PercentOutput, -Ystick + Xstick);
+
+    }
+
+  }
+
+  public void teleopOperationModeTwo() {
+
+    // Default speed
+    double Speed = .10;
+
+    // Macros
+    if (m_bigstickboi.getRawButton(6) || m_bigstickboi.getRawButton(3)) {
+
+      Speed = 1;
+
+    } else if (m_bigstickboi.getRawButton(7)) {
+
+      Speed = .15;
+
+    } else if (m_bigstickboi.getRawButton(10)) {
+
+      Speed = .25;
+
+    } else if (m_bigstickboi.getRawButton(12)) {
+
+      Speed = .5;
+
+    } else if (m_bigstickboi.getRawButton(2)) {
+
+      Spin.Spin180(1);
+
+    } else if (m_bigstickboi.getRawButton(4)) {
+
+      Spin.Spin90(2);
+
+    } else if (m_bigstickboi.getRawButton(5)) {
+
+      Spin.Spin90(1);
+
+    }
+    
+    double Xstick = m_bigstickboi.getRawAxis(0) * Speed;
+    double Ystick = m_bigstickboi.getRawAxis(1) * Speed;
+
+     if (Ystick < 0.2 && Ystick > -0.2) {
+
+      Ystick = 0;
+
+    }
+
+    if (Xstick < 0.2 && Xstick > -0.2) {
+
+      Xstick = 0;
+
+    } 
+
+    RightFront.set(ControlMode.PercentOutput, Ystick + Xstick);
+    LeftFront.set(ControlMode.PercentOutput, -Ystick + Xstick);
+    RightBack.set(ControlMode.PercentOutput, Ystick + Xstick);
+    LeftBack.set(ControlMode.PercentOutput, -Ystick + Xstick);
+
+  }
+
+  /**
+   * This function is called periodically during test mode.
+   */
   @Override
   public void testPeriodic() {
 
@@ -272,6 +485,5 @@ public class Robot extends TimedRobot {
     LeftBack.set(ControlMode.PercentOutput, -Ystick + Xstick);
 
     }
-  }
 
-}
+  }
